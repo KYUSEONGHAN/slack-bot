33d7897eaef88caeb740a4587d949214a77d1c17
KYUSEONGHAN <tyty587587@gmail.com>
2022-03-09 15:23:16 +0900

    2022.3.9.수: 네이버 코로나 확진자 정보 가져오기

--- crawling/corona.py
+++ crawling/corona.py
@@ -2,16 +2,18 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 import requests as re
 
-today = datetime.today().strftime("%Y-%m-%d")
+def get_corona_info():
+    today = datetime.today().strftime("%Y-%m-%d")
 
-# 네이버 코로나 확진자 페이지
-url = 'https://search.naver.com/search.naver?where=nexearch&sm=tab_etc&qvt=0&query=%EC%BD%94%EB%A1%9C%EB%82%9819'
+    # 네이버 코로나 확진자 페이지
+    url = 'https://search.naver.com/search.naver?where=nexearch&sm=tab_etc&qvt=0&query=%EC%BD%94%EB%A1%9C%EB%82%9819'
 
-html = re.get(url)
-soup = BeautifulSoup(html.text, 'html.parser')
+    html = re.get(url)
+    soup = BeautifulSoup(html.text, 'html.parser')
 
-corona_info = soup.select_one("div.status_info").text.strip()
-area_corona = "".join([x.text.strip() for x in soup.select("table.table > tbody")])
+    corona_info = soup.select_one("div.status_info").text.strip()
+    area_corona = "".join([x.text.strip() for x in soup.select("table.table > tbody")])
 
-print(corona_info)
-print(area_corona)
+    message = '날짜: ' + today + '\n' + corona_info + '\n' + area_corona
+
+    return message
