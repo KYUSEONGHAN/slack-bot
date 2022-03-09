ee69b992046074ee01bc6829ea26754e51952847
KYUSEONGHAN <tyty587587@gmail.com>
2022-03-09 17:22:33 +0900

    2022.3.9.수: slack-python 연동

---
+++ slack/message.py
@@ -0,0 +1,10 @@
+import requests
+
+token = '~~'
+
+def post_message(token, channel, text):
+    response = requests.post("https://slack.com/api/chat.postMessage",
+                             headers={"Authorization": "Bearer " + token},
+                             data={"channel": channel, "text": text}
+                             )
+    print(response)
